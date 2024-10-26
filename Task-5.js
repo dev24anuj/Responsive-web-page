2cae08294c19004e2298db6381acb869bb0ee511
dev24anuj <anujbhardwaj1020@gmail.com>
2024-10-26 15:56:49 +0530

    web page

---
+++ Task-5.js
@@ -0,0 +1,29 @@
+// TASK-5
+// API Integration
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetchPosts();
+});
+
+function fetchPosts() {
+  fetch("https://jsonplaceholder.typicode.com/users/1/posts")
+    .then((response) => response.json())
+    .then((data) => displayPosts(data))
+
+    .catch((error) => console.error("Error fetching posts:", error));
+}
+
+function displayPosts(posts) {
+  const postsContainer = document.getElementById("container");
+  posts.forEach((post) => {
+    const postElement = document.createElement("div");
+    postElement.className = "post";
+    postElement.innerHTML = `
+  
+            <h2>${post.title}</h2>
+            <p>${post.body}</p>
+            
+        `;
+    postsContainer.appendChild(postElement);
+  });
+}
